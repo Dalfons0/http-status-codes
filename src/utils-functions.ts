2dc595fa99bf102132d8b89b0d7d1a78b59e079f
David Alfonso <david.alfonso@Lansweeper.com>
2024-05-17 12:26:34 +0200

    Optional error throw in utils functions

--- src/utils-functions.ts
+++ src/utils-functions.ts
@@ -3,34 +3,42 @@ import {
   reasonPhraseToStatusCode,
 } from './utils';
 
-/**
- * Returns the reason phrase for the given status code.
- * If the given status code does not exist, an error is thrown.
- *
- * @param {number|string} statusCode The HTTP status code
- * @returns {string} The associated reason phrase (e.g. "Bad Request", "OK")
- * */
-export function getReasonPhrase(statusCode: (number | string)): (string) {
-  const result = statusCodeToReasonPhrase[statusCode.toString()];
+const checkResult = <T>(result: T, message: string): T => {
   if (!result) {
-    throw new Error(`Status code does not exist: ${statusCode}`);
+    throw new Error(message);
   }
+
   return result;
 }
 
 /**
+ * Returns the reason phrase for the given status code.
+ * If the given status code does not exist, an error is thrown if the flag throwWhenMissing is set up to true,
+ * otherwise return undefined.
+ *
+ * @param {number|string} statusCode The HTTP status code
+ * @param {boolean} [throwWhenMissing=true] Flag to indicate that an error should be thrown if the status code provided is missing 
+ * @returns {string} The associated reason phrase (e.g. "Bad Request", "OK")
+ * */
+export function getReasonPhrase(statusCode: (number | string), throwWhenMissing = true): (string) {
+  const result = statusCodeToReasonPhrase[statusCode.toString()];
+
+  return throwWhenMissing ? checkResult(result, `Status code does not exist: ${statusCode}`) : result;
+}
+
+/**
  * Returns the status code for the given reason phrase.
- * If the given reason phrase does not exist, undefined is returned.
+ * If the given reason phrase does not exist, an error is thrown if the flag throwWhenMissing is set up to true,
+ * otherwise return undefined.
  *
  * @param {string} reasonPhrase The HTTP reason phrase (e.g. "Bad Request", "OK")
+ * @param {boolean} [throwWhenMissing=true] Flag to indicate that an error should be thrown if the reason phrase is missing
  * @returns {string} The associated status code
  * */
-export function getStatusCode(reasonPhrase: string): (number) {
+export function getStatusCode(reasonPhrase: string, throwWhenMissing = true): (number) {
   const result = reasonPhraseToStatusCode[reasonPhrase];
-  if (!result) {
-    throw new Error(`Reason phrase does not exist: ${reasonPhrase}`);
-  }
-  return result;
+
+  return throwWhenMissing ? checkResult(result, `Reason phrase does not exist: ${reasonPhrase}`) : result;
 }
 
 /**
